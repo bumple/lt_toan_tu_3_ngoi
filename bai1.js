5099f0305636d43c34f471de7375b94fdb1bf77b
bumple <81680183+bumple@users.noreply.github.com>
2021-04-07 20:49:04 +0700

    first commit

---
+++ bai1.js
@@ -0,0 +1,5 @@
+let a = parseInt(prompt("nhap gia tri a"))
+let b = parseInt(prompt("nhap gia tri b"))
+
+result = (a + b <4) ? "below" : "Over";
+alert("ket qua la: " + result)
---
+++ bai2.js
@@ -0,0 +1,9 @@
+let a = prompt("Nhap gia tri vao day")
+let m = (a === 'Employee') ?
+    'Hello' :
+    (a === "Director") ?
+        "Greeting" :
+        (login === "") ?
+            'No login' :
+            '';
+alert(m)
